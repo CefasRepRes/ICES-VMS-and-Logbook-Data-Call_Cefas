ab23152789ac057279547f21bf527b60ef7407d7
md09 <michael.day@cefas.gov.uk>
2024-07-12 14:48:18 +0100

    remove test lines and changes to filepath outputs

--- 2_eflalo_tacsat_analysis.R
+++ 2_eflalo_tacsat_analysis.R
@@ -7,7 +7,7 @@
 
 setwd("C:/Users/MD09/OneDrive - CEFAS/projects/datacalls/ices/2024")
 
-source("C:\\Users\\MD09\\OneDrive - CEFAS\\projects\\datacalls\\ices\\2024\\global-subset.R")
+source("C:\\Users\\MD09\\Documents\\git\\ICES-VMS-and-Logbook-Data-Call_Cefas\\global-subset.R")
 
 
 
@@ -23,10 +23,6 @@ for(year in yearsToSubmit){
   load(file = paste0(outPath, paste0("/cleanEflalo",year,".RData")) )
   load(file = paste0(outPath, paste0("/cleanTacsat", year,".RData")) )
   
-  tacsat %>% distinct(FT_REF) %>% tally()
-  eflalo %>% distinct(FT_REF) %>% tally()
-  
-  eflalo %>% filter(FT_REF == 610917780)
   
   # Assign geometry column to tacsat for later operations
   tacsat$geometry <- NULL
@@ -60,16 +56,12 @@ for(year in yearsToSubmit){
     tacsatp[[col]] <- eflalo[[col]][match(as.numeric(tacsatp$FT_REF), as.numeric(eflalo$FT_REF))]
   }
   
-  tacsatp %>%  filter(  LE_GEAR %>%  is.na())  %>% distinct (FT_REF)
   
-  eflalo %>% filter( FT_REF == '610838243')
-  tacsatp %>%  filter( SI_FT == '610838243')
-
-  tacsatp <- data.frame(tacsatp)
+  tacsatp <- as.data.frame(tacsatp)
   
   # Save not merged tacsat data
   # Subset 'tacsatp' where 'FT_REF' equals 0 (not merged)
-  tacsatp$FT_REF <- tacsatp$SI_FT
+  # tacsatp$FT_REF <- tacsatp$SI_FT
   tacsatpmin <- subset(tacsatp, FT_REF == 0)
   
   # Feedback on tacsatpmin
@@ -91,45 +83,46 @@ for(year in yearsToSubmit){
   # tacsatp <- tacsatp_bk
   
   
-  tacsatpa_LE_GEAR <- trip_assign(tacsatp, eflalo, col = "LE_GEAR", trust_logbook = T) 
+  tacsatpa_LE_GEAR <- trip_assign(tacsatp, eflalo, col = "LE_GEAR", trust_logbook = T)
   tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_GEAR$FT_REF,], tacsatpa_LE_GEAR), fill = T)
-  
+
   dim(tacsatpa_LE_GEAR)
   dim(tacsatp)
-  
-  
+
+
+
   tacsatp %>% filter(is.na(LE_GEAR)) %>% tally()
-  
+
   tacsatp %>%  filter( FT_REF == '610917780')
-  
+
   eflalo %>% filter ( FT_REF == 'c')
-  
+
   tacsatp$FT_REF <- as.numeric(tacsatp$FT_REF)
   eflalo$FT_REF <- as.numeric(eflalo$FT_REF)
-  
-  
+
+
   tacsatpa_LE_MSZ <- trip_assign(tacsatp, eflalo, col = "LE_MSZ", trust_logbook = T)
   tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_MSZ$FT_REF,], tacsatpa_LE_MSZ), fill = T)
-  
+
   tacsatpa_LE_RECT <- trip_assign(tacsatp, eflalo, col = "LE_RECT", trust_logbook = T)
   tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_RECT$FT_REF,], tacsatpa_LE_RECT), fill = T)
-  
+
   tacsatpa_LE_MET <- trip_assign(tacsatp, eflalo, col = "LE_MET", trust_logbook = T)
   tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_MET$FT_REF,], tacsatpa_LE_MET), fill = T)
-  
-  
-  tacsatp %>% filter (is.na( LE_GEAR ) )   %>% left_join(eflalo %>% rename(eLE_GEAR = LE_GEAR), by = "FT_REF" ) %>% 
-    mutate (   LE_GEAR  = eLE_GEAR )     
-  
-  
+
+
+  tacsatp %>% filter (is.na( LE_GEAR ) )   %>% left_join(eflalo %>% rename(eLE_GEAR = LE_GEAR), by = "FT_REF" ) %>%
+    mutate (   LE_GEAR  = eLE_GEAR )
+
+
   [tacsatp$FT_REF %!in% tacsatpa_LE_GEAR$FT_REF, LE_GEAR := ]
-   
-  
+
+
   tacsatp$LE_GEAR     = eflalo$LE_GEAR    [ match(tacsatp$FT_REF, eflalo$FT_REF)]
   tacsatp$LE_MSZ      = eflalo$LE_MSZ     [ match(tacsatp$FT_REF, eflalo$FT_REF)]
   tacsatp$LE_RECT     = eflalo$LE_RECT    [ match(tacsatp$FT_REF, eflalo$FT_REF)]
   tacsatp$LE_MET      = eflalo$LE_MET     [ match(tacsatp$FT_REF, eflalo$FT_REF)]
-  
+
   if("LE_WIDTH" %in% names(eflalo)){
     tacsatpa_LE_WIDTH <- trip_assign(tacsatp, eflalo, col = "LE_WIDTH", trust_logbook = T)
     tacsatp <- rbindlist(list(tacsatp[tacsatp$FT_REF %!in% tacsatpa_LE_WIDTH$FT_REF,], tacsatpa_LE_WIDTH), fill = T)
@@ -173,30 +166,46 @@ for(year in yearsToSubmit){
   idx_euro <- grep("LE_EURO_", colnames(eflalo)[colnames(eflalo) %!in% c("LE_EURO_TOT")])
   
   # Calculate the total KG and EURO for each row
-  if("LE_KG_TOT" %!in% names(eflalo))
+  if("LE_KG_TOT" %!in% names(eflalo)){
     eflalo$LE_KG_TOT <- rowSums(eflalo[, idx_kg], na.rm = TRUE)
-  if("LE_EURO_TOT" %!in% names(eflalo))
+  }
+  if("LE_EURO_TOT" %!in% names(eflalo)){
     eflalo$LE_EURO_TOT <- rowSums(eflalo[, idx_euro], na.rm = TRUE)
+  }
+  
+  eflalo_bk <- eflalo
+  eflalo <- eflalo_bk
   
   # Remove the columns used for the total calculation
-   eflalo <- eflalo[, -c(idx_kg, idx_euro)]
+  eflalo <- eflalo[, -c(idx_kg, idx_euro)]
   
   # Split eflalo into two data frames based on the presence of FT_REF in tacsatp
   eflaloNM <- subset(eflalo, !FT_REF %in% unique(tacsatp$FT_REF))
   eflaloM <- subset(eflalo, FT_REF %in% unique(tacsatp$FT_REF))
 
   message(sprintf("%.2f%% of the eflalo data not in tacsat\n", (nrow(eflaloNM) / (nrow(eflaloNM) ))))
-
+  
+  
+  tacsatp$SI_STATE <- ifelse(tacsatp$SI_SP >1 & tacsatp$SI_SP <=6, 'f', 's')
+  
+  tacsatp %>% filter(SI_STATE == 'f') %>% tally()
+    
   # Convert SI_STATE to binary (0/1) format
   tacsatp$SI_STATE <- ifelse(tacsatp$SI_STATE == "f", 1, 0)
   
   # Filter rows where SI_STATE is 1
-  tacsatEflalo <- tacsatp[tacsatp$SI_STATE == 1,]
+  # tacsatEflalo <- tacsatp[tacsatp$SI_STATE == 1,]
+  tacsatp <- tacsatp[tacsatp$SI_STATE == 1,]
   
   tacsatp <- tacsatp[!is.na(tacsatp$INTV),]
   
+  typeof(tacsatp$SI_DATE)
+  typeof(eflalo$LE_CDAT)
+  
+  eflalo$LE_CDAT <- as.character(eflalo$LE_CDAT)
+  
   # Distribute landings among pings, first by day, metier and trip; then by metier and trip; then by trip
-  tacsatEflalo <- splitAmongPings2(tacsatp, eflalo)
+  tacsatEflalo <- splitAmongPings2(tacsatp, eflalo) # was originally splitAmongPings2(tacsatp, eflalo)
   
   eflalo$tripInTacsat <- ifelse(eflalo$FT_REF %in% tacsatEflalo$FT_REF, "Y", "N")
 
@@ -208,7 +217,12 @@ for(year in yearsToSubmit){
 
   save(
     eflalo,
-    file = file.path(outPath, paste0("/cleanEflalo", year, ".RData"))
+    file = file.path(outPath, paste0("/cleanEflalo2", year, ".RData"))
+  )
+  
+  save(
+    tacsatp,
+    file = file.path(outPath, paste0("/tacsatp", year, ".RData"))
   )
   
   
